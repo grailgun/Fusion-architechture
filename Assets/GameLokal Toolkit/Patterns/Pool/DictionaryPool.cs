aacadf7c0dddc7439d1cec84b939176ec5ff9661
grailgun <sinatryarizqi.m@gmail.com>
2022-07-27 17:18:01 +0700

    Add gamelokal

---
+++ Assets/GameLokal Toolkit/Patterns/Pool/DictionaryPool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GameLokal.Toolkit
+{
+  /// <summary>
+  ///   <para>A version of Pool.CollectionPool_2 for Dictionaries.</para>
+  /// </summary>
+  public class DictionaryPool<TKey, TValue> : 
+    CollectionPool<Dictionary<TKey, TValue>, KeyValuePair<TKey, TValue>>
+  {
+  }
+}
---
+++ Assets/GameLokal Toolkit/Patterns/Pool/HashSetPool.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GameLokal.Toolkit
+{
+  /// <summary>
+  ///   <para>A version of Pool.CollectionPool_2 for HashSets.</para>
+  /// </summary>
+  public class HashSetPool<T> : CollectionPool<HashSet<T>, T>
+  {
+  }
+}
